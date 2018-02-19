ad59f8219c8b50c244670ba0ff172c5dd948f115
John Sutherland <jrms999@gmail.com>
2018-02-19 13:33:54 +0000

    Add files via upload

---
+++ slick-carousel-vertical-mouse-wheel-scroll/js/index.js
@@ -0,0 +1,21 @@
+var $slider = $("#slider");
+$slider.on('init', function () {
+	mouseWheel($slider);
+}).slick({
+	dots: true,
+	vertical: true,
+	infinite: false
+});
+function mouseWheel($slider) {
+	$(window).on('wheel', { $slider: $slider }, mouseWheelHandler);
+}
+function mouseWheelHandler(event) {
+	event.preventDefault();
+	var $slider = event.data.$slider;
+	var delta = event.originalEvent.deltaY;
+	if (delta > 0) {
+		$slider.slick('slickPrev');
+	} else {
+		$slider.slick('slickNext');
+	}
+}
